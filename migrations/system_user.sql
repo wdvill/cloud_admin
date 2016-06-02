ff318c33e753e77c28a05140eaf3299ccfe49218
wdvill <wllabs@163.com>
2016-06-02 16:50:32 +0800

    fix sql format

--- migrations/system_user.sql
+++ migrations/system_user.sql
@@ -1,12 +1,12 @@
 
 CREATE TABLE `system_user` (
-  `uid` int(10) UNSIGNED NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
   `user` varchar(50) NOT NULL,
   `passwd` varchar(50) NOT NULL,
   `create_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `system_user` CHANGE `uid` `uid` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `system_user` CHANGE `id` `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `system_user`
-  ADD PRIMARY KEY (`uid`);
+  ADD PRIMARY KEY (`id`);
